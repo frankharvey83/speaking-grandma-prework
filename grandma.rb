86c43c5e2e3cf7b04bedaed5d0c8f119c42fd0e8
Frank H Mcmanus <frank@thefrankharvey.com>
2018-02-26 22:54:12 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,6 +6,8 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
 end
 
 # Whatever you say to grandma, she should respond with
